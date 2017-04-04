eb29c58f5d0748ab4b0b9eb94e8962d31adb6fa8
Andrey <andrey.vladykov@gmail.com>
2017-04-05 00:12:22 +0300

    Add ValueConveter

---
+++ AutoFixture.Extensions/ValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AutoFixture.Extensions
+{
+    public abstract class ValueConverter<TSource, TTarget> : IValueConverter
+        where TSource : class
+        where TTarget : class
+    {
+        public object Convert(object value)
+        {
+            var sourceValue = value as TSource;
+            if (sourceValue == null)
+            {
+                throw new ArgumentException(
+                    $"Argument of type {typeof(TSource).FullName} was expected, but {value.GetType().FullName} was received.");
+            }
+
+            return Convert(sourceValue);
+        }
+
+        protected abstract TTarget Convert(TSource value);
+    }
+}
