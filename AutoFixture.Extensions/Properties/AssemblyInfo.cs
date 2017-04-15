3dd292acb91c89f198036f86d1a588ed5cdbe23d
Andrey <andrey.vladykov@gmail.com>
2017-04-15 20:45:48 +0300

    Add strong naming

--- AutoFixture.Extensions/Properties/AssemblyInfo.cs
+++ AutoFixture.Extensions/Properties/AssemblyInfo.cs
@@ -14,6 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if SIGN
+[assembly: AssemblyKeyFile("key.snk")]
+#endif
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
